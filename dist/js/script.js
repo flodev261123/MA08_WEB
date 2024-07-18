c36cc6c209a3b879c312fad3eb3bc05fd2e4d602
alip <haikalsialip999@gmail.com>
2024-07-19 06:26:34 +0700

    Initial commit

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+
+
+// Navbars Fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixnav = header.offsetTop;
+    if (window.pageYOffset > fixnav) {
+        header.classList.add('navbar-fixed');
+    }
+     else{
+        header.classList.remove('navbar-fixed');
+
+    }
+}
